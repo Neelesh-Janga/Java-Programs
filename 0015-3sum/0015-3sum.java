c003b19e2b04c32bc84a3161b76c5f85340aab18
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-05 12:30:49 +0530

    Time: 226 ms (30.96%), Space: 50.9 MB (41.7%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], i);
+        }
+
+        for(int i = 0; i < nums.length-2; i++){
+            if(nums[i] > 0){
+                break;
+            }
+
+            for(int j = i+1; j < nums.length-1; j++){
+                
+                int k = -(nums[i] + nums[j]);
+                
+                if(map.containsKey(k) && map.get(k) > j){
+                    result.add(Arrays.asList(nums[i], nums[j], k));
+                }
+
+                j = map.get(nums[j]);
+            }
+
+            i = map.get(nums[i]);
+        }
+
+        return result;
+    }
+}
