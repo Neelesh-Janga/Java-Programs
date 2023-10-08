186a07d77dd086385bf42b63340232b5492c47a4
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-08 12:02:17 +0530

    Time: 1 ms (98.68%), Space: 40.2 MB (90.71%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,10 +4,9 @@ class Solution {
     public boolean isValid(String s) {
         char [] arr = new char[s.length()];
         int k = -1;
-        StringBuffer sb = new StringBuffer(s);
 
         for(int i = 0; i < s.length(); i++) {
-            char token = sb.charAt(i);
+            char token = s.charAt(i);
 
             if(token == '{' || token == '[' || token == '('){
                 arr[++k] = token;
@@ -16,17 +15,19 @@ class Solution {
             if(token == '}' || token == ']' || token == ')'){
                 if(k < 0)
                     return false;
-
+        
                 char c = arr[k];
-                if((token == ')' && c == '(') || (token == ']' && c == '[') || (token == '}' && c == '{')) {
+                
+                if(
+                    (token == ')' && c == '(') || 
+                    (token == ']' && c == '[') || 
+                    (token == '}' && c == '{')) {
                     k--;
                 }else{
                     return false;
                 }
             }
         }
-
-        System.gc();
 
         return k == -1 ? true : false;
     }
