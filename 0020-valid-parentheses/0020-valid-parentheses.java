f56d7992f787c8faeab8adb26eb7085c021279f4
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-08 11:58:18 +0530

    Time: 1 ms (98.68%), Space: 40.4 MB (68.97%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        char [] arr = new char[s.length()];
+        int k = -1;
+        StringBuffer sb = new StringBuffer(s);
+
+        for(int i = 0; i < s.length(); i++) {
+            char token = sb.charAt(i);
+
+            if(token == '{' || token == '[' || token == '('){
+                arr[++k] = token;
+            }
+
+            if(token == '}' || token == ']' || token == ')'){
+                if(k < 0)
+                    return false;
+                    
+                char c = arr[k];
+                if((token == ')' && c == '(') || (token == ']' && c == '[') || (token == '}' && c == '{')) {
+                    k--;
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return k == -1 ? true : false;
+    }
+}
