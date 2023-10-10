fc850cd3635819de0040b159a49e5fd5333c5a55
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-10 20:27:56 +0530

    Time: 0 ms (100%), Space: 40.5 MB (90.38%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int hold = 0;
+        for(int i = digits.length-1; i >= 0; i--){
+            if(digits[i] == 9){
+                hold = 1;
+                digits[i] = 0;
+            }else{
+                hold = 0;
+                digits[i]++;
+                break;
+            }
+        }
+
+        
+        if(hold == 1){
+            int [] newDigits = new int[digits.length + 1];
+            newDigits[0] = hold;
+            for(int i = digits.length; i > 0; i--){
+                newDigits[i] = digits[i-1];
+            }
+            return newDigits;
+        }
+
+        return digits;
+    }
+}
