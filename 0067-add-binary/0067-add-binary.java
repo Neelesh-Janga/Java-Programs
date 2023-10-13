6c538db623b2ec5c3f24da5b27b3c0ff33ddeebe
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-13 14:46:05 +0530

    Time: 7 ms (12.69%), Space: 43.4 MB (15.27%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,38 +1,30 @@
 class Solution {
     public String addBinary(String a, String b) {
-        int hold = 0;
-        int pl = a.length() <= b.length() ? b.length()-1 : a.length()-1;
-        int ps = a.length() < b.length() ? a.length()-1 : b.length()-1;
-        String res = "", longStr = a.length() <= b.length() ? b : a, shortStr = a.length() <= b.length() ? a : b;
-        
-        while(true){
-            if(pl < 0 && ps < 0){
-                return hold == 1 ? "1" + res : res;
-            }
-            else if(pl >= 0 && ps >= 0){
-                if(longStr.charAt(pl) == '0' && shortStr.charAt(ps) == '0'){
-                    res = hold + res;
-                    hold = 0;
-                }else if(longStr.charAt(pl) == '1' && shortStr.charAt(ps) == '1'){
-                    res = hold + res;
-                    hold = 1;
-                }else if(longStr.charAt(pl) != shortStr.charAt(ps)){
-                    res = (hold == 0 ? 1 : 0) + res;
-                }
-                pl--;
-                ps--;
-            }else{
-                if(longStr.charAt(pl) == '0'){
-                    res = hold + res;
-                    hold = 0;
-                }
-                else if(longStr.charAt(pl) == '1'){
-                    res = (hold == 0 ? 1 : 0) + res;
-                }
-                pl--;
-            }
+        int carry = 0;
+        int p1 = a.length()-1;
+        int p2 = b.length()-1;
+        int sum = 0;
+        String res = "";
 
-            System.out.print(hold);
+        while(p1 >= 0 || p2 >= 0 || carry == 1){
+            // '0' - 48, '1' - 49, '2' - 
+            char c1 = (p1 >= 0 ? a.charAt(p1) : '0');
+            char c2 = (p2 >= 0 ? b.charAt(p2) : '0');
+            
+            sum = c1 + c2 + carry;
+            
+            if(sum > 48 + 49){
+                carry = 1;
+                res = (sum - 2 * 49) + res;
+            }else {
+                carry = 0;
+                res = (sum - 2 * 48) + res;
+            }
+            
+            p1--;
+            p2--;
         }
+
+        return res;
     }
 }
