320645e0dd5fc7fb69424c0bfb16b8dcd965593f
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-14 10:57:15 +0530

    Time: 0 ms (100%), Space: 39.8 MB (26.63%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,14 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n <= 1){
-            return n == 1 ? true : false;
-        }else{
-            return 
-            Math.log10((long)n) / Math.log10(2) 
-            == 
-            (long)(Math.log10((long)n) / Math.log10(2)) 
-            ? 
-            true : false;
-        }
+        return n > 0 && ((n & (n-1)) == 0);
     }
 }
