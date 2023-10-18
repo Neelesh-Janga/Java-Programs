c9b2be5fd673723677db7b008b4e2a940633a24b
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-18 09:34:30 +0530

    Time: 9 ms (11.36%), Space: 42.5 MB (76.01%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-
-        if(x < 0){
-            return false;
-        }
-
-        int num = x, res = 0;
-
-        while(num != 0){
-            int rem = num % 10;
-            res = res * 10 + rem;
-            num /= 10;
-        }
-
-        return x == res ? true : false;
+        StringBuffer sb = new StringBuffer(x+"");
+        return (x+"").equals(sb.reverse().toString());
     }
 }
