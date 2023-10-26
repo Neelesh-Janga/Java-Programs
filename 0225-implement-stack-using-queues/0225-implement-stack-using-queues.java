85dfddb8280a07979a2e6e831d6c2907ff5b9adb
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-10-26 14:33:23 +0530

    Time: 0 ms (100%), Space: 40.2 MB (45.21%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -0,0 +1,42 @@
+class MyStack {
+
+    private ArrayList<Integer> queue;
+    int low = -1;
+    int high = -1;
+
+    public MyStack() {
+        queue = new ArrayList<>();    
+    }
+    
+    public void push(int x) {
+        queue.add(x);
+        high++;
+    }
+    
+    public int pop() {
+        if(high == -1) return 0;
+
+        int num = queue.get(high);
+        queue.remove(high--);
+        return num;
+    }
+    
+    public int top() {
+        if(high == -1) return 0;
+        
+        return queue.get(high);
+    }
+    
+    public boolean empty() {
+        return high == -1;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
