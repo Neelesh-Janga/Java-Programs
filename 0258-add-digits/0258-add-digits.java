00e6a4bc9082adc1d72a0bc2acedcfbb38e70c5b
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-11-20 10:50:04 +0530

    TC=O(1) - Time: 1 ms (88.84%), Space: 39.9 MB (25.65%)

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,7 +1,5 @@
 class Solution {
     public int addDigits(int num) {
-        if(num < 10) return num;
-
-        return addDigits((num % 10) + addDigits(num/10));
+        return num == 0 ? 0 : (num % 9 == 0 ? 9 : num % 9);
     }
 }
