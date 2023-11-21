895e51f9df0fa0baa1d76708c73105e363d5f4d7
Neelesh Janga <139889521+Neelesh-Janga@users.noreply.github.com>
2023-11-21 11:09:27 +0530

    Time: 0 ms (100%), Space: 39.42 MB (52.35%)

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isHappy(int n) {
-        int fastSum = n;
-        int slowSum = n;
+        if(n < 10){
+            if(n == 1 || n == 7) return true;
+            else return false;
+        }
 
-        do{
-            fastSum = calculateSum(calculateSum(fastSum));
-            slowSum = calculateSum(slowSum);
-            if(slowSum == 1 || fastSum == 1) return true;
-        }while (fastSum != slowSum);
-
-        return false;
+        return isHappy(calculateSum(n));
     }
 
     public int calculateSum(int n){
         int sum = 0;
         while(n > 0){
-            sum += Math.pow(n % 10, 2);
+            sum += ((n % 10) * (n % 10));
             n /= 10;
         }
         return sum;
