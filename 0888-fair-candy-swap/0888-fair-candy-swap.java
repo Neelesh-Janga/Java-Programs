e7b684dc46ba222cf7bc2e08177c5a16377fc665
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-12-21 09:47:56 +0530

    Time: 2 ms (100%), Space: 45.91 MB (7.54%)
    Best Time Complexity Solution

--- 0888-fair-candy-swap/0888-fair-candy-swap.java
+++ 0888-fair-candy-swap/0888-fair-candy-swap.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
         int aliceSum=0, bobSum=0, diffAvg;
-        Set<Integer> set = new HashSet();
+        byte[] arr = new byte[1_00_001];
 
         for(int i = 0; i < aliceSizes.length; i++){
             aliceSum = aliceSum + aliceSizes[i];
@@ -9,14 +9,15 @@ class Solution {
 
         for(int i = 0; i < bobSizes.length; i++){
             bobSum = bobSum + bobSizes[i];
-            set.add(bobSizes[i]);
+            arr[bobSizes[i]] = 1;
         }
 
         diffAvg = (aliceSum - bobSum)/2;
 
-        for(int i = 0; i < aliceSizes.length; i++){
-            if(set.contains(aliceSizes[i] - diffAvg))
-                return new int[] {aliceSizes[i], aliceSizes[i] - diffAvg};
+        for(int i = 0, required; i < aliceSizes.length; i++){
+            required = aliceSizes[i] - diffAvg;
+            if(required >= 0 && required < 1_00_001 && arr[required] == 1)
+                return new int[] {aliceSizes[i], required};
         }
 
         return new int[]{-1, -1};
