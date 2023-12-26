29bf4ee7a48eb618ace69480ef232ff1675cf4a6
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-12-26 22:45:01 +0530

    Time: 0 ms (100%), Space: 51.6 MB (5.69%)

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.java
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.java
@@ -15,20 +15,9 @@
  */
 class Solution {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        int sum = 0;
-        
-        if (root == null) {
-            return 0;
-        }
-        if (root.val >= low && root.val <= high) {
-            sum = sum + root.val;
-        }
-        if (root.val > low) {
-            sum = sum + rangeSumBST(root.left, low, high);
-        }
-        if (root.val < high) {
-            sum = sum + rangeSumBST(root.right, low, high);
-        }
-        return sum;
+        if(root == null) return 0;
+        return (root.val >= low && root.val <= high ? root.val : 0)
+                + (root.val > low ? rangeSumBST(root.left, low, high) : 0)
+                + (root.val < high ? rangeSumBST(root.right, low, high) : 0);
     }
 }
