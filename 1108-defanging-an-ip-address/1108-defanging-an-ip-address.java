c22ca11a6747a691590842fbb8f2df497c86f820
Neelesh-Janga <codelikeneelesh@gmail.com>
2023-12-30 23:03:02 +0530

    Time: 0 ms (100%), Space: 41.4 MB (7.7%)

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,12 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < address.length(); i++){
-            char c = address.charAt(i);
-            sb.append(c == '.' ? "[.]" : c);
-        }
-
-        return sb.toString();
+        return address.replace("." , "[.]");
     }
 }
